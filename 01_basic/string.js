a0480ce6099faf8971f64faf56cbbc7a95347515
Waghmare Sneha Virendra <147601312+sneha-waghmare@users.noreply.github.com>
2024-09-30 16:43:49 +0000

    add some changes on string.js file

---
+++ 01_basic/string.js
@@ -0,0 +1,10 @@
+// string
+// there are two ways to declare string
+
+let name1 = "sneha"
+let name2 = new String("sneha")
+console.log(name1)                  //sneha
+console.log(name2)                  // [String: 'sneha']
+
+console.log("marks"+ " is " + 99 +" to " + name1)
+console.log("marks is 99 to ${name1}")
