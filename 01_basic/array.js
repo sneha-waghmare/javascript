55c605925b33f56a8924eb84053826867cb03ff8
Waghmare Sneha Virendra <147601312+sneha-waghmare@users.noreply.github.com>
2024-10-02 17:19:11 +0000

    add array.js file

---
+++ 01_basic/array.js
@@ -0,0 +1,19 @@
+//array
+const myArr = [0,1,2,3,4,5,"sneha",true]
+const myArr2 = new Array(1,2,3,4,5)
+
+console.log(myArr)
+console.log(myArr[6])
+console.log(myArr2)
+
+//array methods
+
+//push(), pop()
+myArr.push(10)
+myArr.push(15)
+console.log(myArr)
+myArr.pop()
+console.log(myArr)
+
+
+//unshift(), 
